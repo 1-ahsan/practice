c4d79e8049034f60193bc766c457001a3f35886c
Ahsan <ahsanabas.info@gmail.com>
2025-12-31 19:34:42 +0500

    added check box in lifecycle class

--- lib/lifecycle.dart
+++ lib/lifecycle.dart
@@ -11,11 +11,38 @@ class _lifecycleStates extends State<lifecycle>{
 
   late TextEditingController controller ;
 
+  bool _ischeck = false;
+  bool _listtile = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     controller = TextEditingController();
+  }
+
+  Widget _checkboxx(){
+    return Checkbox(
+        value: _ischeck,
+        activeColor: Colors.blue,
+        onChanged: (bool? value){
+          setState(() {
+            _ischeck = value!;
+          });
+        },
+    );
+  }
+
+  Widget _checklist(){
+   return CheckboxListTile(
+     title: Text("What is 3+5?"),
+       value: _listtile,
+       onChanged: (bool? neww){
+          setState((){
+            _listtile = neww!;
+          });
+        }
+   );
   }
 
   @override
@@ -27,14 +54,20 @@ class _lifecycleStates extends State<lifecycle>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Container(
-        child: TextField(
-          controller: controller,
-          decoration: InputDecoration(
-            labelText: "Enter your text",
-            border: OutlineInputBorder(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: "Enter your text",
+              border: OutlineInputBorder(),
+            ),
           ),
-        ),
+          _checkboxx(),
+          _checklist(),
+        ],
       ),
     );
   }
